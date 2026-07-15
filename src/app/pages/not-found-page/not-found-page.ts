531d623f13467c6093378fbc9660d6d052a94ec5
NMakarevich <nikoladze529@mail.ru>
2026-07-15 19:56:14 +0300

    feat: add not found page

--- src/app/pages/not-found-page/not-found-page.ts
+++ src/app/pages/not-found-page/not-found-page.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { routesList } from '@shared/routes/routes';
 
 @Component({
   selector: 'app-not-found-page',
-  imports: [],
+  imports: [MatButton, RouterLink],
   templateUrl: './not-found-page.html',
   styleUrl: './not-found-page.scss',
 })
-export class NotFoundPage {}
+export class NotFoundPage {
+  protected readonly routesList = routesList;
+}
