e8117fd50a98b5ac33a38d4001492037eb7c6f88
NMakarevich <nikoladze529@mail.ru>
2026-07-15 19:27:37 +0300

    feat: define app routes

---
+++ src/app/shared/routes/routes.ts
@@ -0,0 +1,31 @@
+import { RoutesList } from '@shared/routes/types';
+
+export const routesList: RoutesList = {
+  main: {
+    path: '',
+    getLink: () => {
+      return '';
+    },
+    title: 'Планировщик задач',
+  },
+  todos: {
+    path: 'todos',
+    getLink: () => {
+      return 'todos';
+    },
+  },
+  todosDate: {
+    path: 'todos/:year-month-date',
+    getLink: (date) => {
+      return `todos/${date}`;
+    },
+    title: 'Задачи',
+  },
+  notFound: {
+    path: 'not-found',
+    getLink: () => {
+      return 'not-found';
+    },
+    title: 'Страница не найдена',
+  },
+};
