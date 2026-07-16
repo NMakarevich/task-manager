30a53d84770ebf6346f075e56aac82ccbc8518ea
NMakarevich <nikoladze529@mail.ru>
2026-07-16 18:55:20 +0300

    feat: add tasks indicator

--- src/app/components/day/day.ts
+++ src/app/components/day/day.ts
@@ -3,10 +3,12 @@ import { ColorizeDay } from '@components/day/colorize-day';
 import { DatePipe, NgClass } from '@angular/common';
 import { Router } from '@angular/router';
 import { CalendarService } from '@services/calendar-service';
+import { TodoService } from '@services/todo-service';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @Component({
   selector: 'app-day',
-  imports: [ColorizeDay, DatePipe, NgClass],
+  imports: [ColorizeDay, DatePipe, NgClass, MatBadgeModule],
   templateUrl: './day.html',
   styleUrl: './day.scss',
 })
@@ -14,6 +16,23 @@ export class Day {
   day = input<Date>();
   private router = inject(Router);
   private calendarService = inject(CalendarService);
+  private todoService = inject(TodoService);
+
+  currentDate = () => {
+    const day = this.day();
+    if (day) {
+      const [month, date, year] = day.toLocaleDateString('en-US').split('/');
+      return `${year}-${month}-${date}`;
+    } else {
+      return '';
+    }
+  };
+
+  countOfTasks = computed(() => {
+    return this.todoService.todos()[this.currentDate()]
+      ? this.todoService.todos()[this.currentDate()].length
+      : 0;
+  });
 
   isSelected = computed(() => {
     return this.calendarService.getDate() === this.day()?.toDateString();
@@ -29,8 +48,7 @@ export class Day {
   navigateToTodo = () => {
     const day = this.day();
     if (day) {
-      const [month, date, year] = day.toLocaleDateString('en-US').split('/');
-      this.router.navigate([`todos/${year}-${month}-${date}`]);
+      this.router.navigate([`todos/${this.currentDate()}`]);
     }
   };
 }
